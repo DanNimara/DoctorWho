e565570425576a98abbae9b66536b5edb354f526
TheDoctor1963 <TheDoctor1963@users.noreply.github.com>
2020-04-03 04:25:37 +0300

    stilizare

---
+++ resurse/js/stickymenu.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+			var height = Math.max( document.body.scrollHeight, document.body.offsetHeight, 
+		document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
+			document.getElementById("stickymenu").style.height=height+"px"
+
+}
