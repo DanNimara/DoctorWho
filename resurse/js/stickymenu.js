d5357763ce467b71b2c8839aba31b427b6b9ea13
TheDoctor1963 <TheDoctor1963@users.noreply.github.com>
2020-04-03 22:06:00 +0300

    animated gallery+sound

--- resurse/js/stickymenu.js
+++ resurse/js/stickymenu.js
@@ -1,6 +1,17 @@
 window.onload=function(){
-			var height = Math.max( document.body.scrollHeight, document.body.offsetHeight, 
-		document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
-			document.getElementById("stickymenu").style.height=height+"px"
+	var height = Math.max( document.body.scrollHeight, document.body.offsetHeight, 
+document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
+	document.getElementById("stickymenu").style.height=height+"px"
+	
+}
 
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
 }
